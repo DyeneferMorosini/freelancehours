c6d1f0cd0c17bb669355b5605e2b278c6c9232da
Dyenefer <morosinidy@gmail.com>
2025-03-20 16:13:00 -0300

    creation proposals models and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Models\Project;
 use App\Models\User;
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Proposal;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->count(200)->create();
+
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn (User $u) => Project::factory()->create(['created_by' =>$u->id]));
+            ->each(function (User $u) {
+
+                $project = Project::factory()->create(['created_by' =>$u->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+                
+            });
     }
 }
