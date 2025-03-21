c9bf44ed0947db50054216002292b91c043f16cd
Dyenefer <morosinidy@gmail.com>
2025-03-20 21:38:54 -0300

    creation of projects routes

---
+++ resources/views/livewire/projects/index.blade.php
@@ -0,0 +1,11 @@
+<div>
+    @foreach($this->projects as $project)
+
+    <li>
+        <a href="">
+            {{ $project->id }}. {{ $project->title }}
+        </a>
+    </li>    
+
+    @endforeach
+</div>
